d3b7482d2b6afc8d90a8d8cb589781ec6e7062c0
Amin Majlesi <majlesi@amnpardaz.ir>
2022-07-06 17:35:25 +0430

    add navigation try.1

---
+++ app/src/main/java/com/example/movieapp/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.movieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.movieapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setupWithNavController(moviesNavHostFragment.findNavController())
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/ui/fragment/FragmentMovieDetails.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+class FragmentMovieDetails : Fragment(R.layout.fragment_movie_details) {
+
+}
